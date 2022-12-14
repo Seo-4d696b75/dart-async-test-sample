8793063612df6fc64866a8854d60789998c6c6ae
k_senda <k_senda@yumemi.co.jp>
2022-12-14 18:27:53 +0900

    修正：検索結果をランダムに変更

--- lib/data/repository/search_repository.dart
+++ lib/data/repository/search_repository.dart
@@ -1,17 +1,24 @@
+import 'dart:math';
+
 import 'package:async_test_sample/data/model/search_result.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final searchRepositoryProvider = Provider(
-  (_) => SearchRepository(),
+  (_) => SearchRepository(0),
 );
 
 class SearchRepository {
+  SearchRepository(int seed) : _random = Random(seed);
+
+  final Random _random;
+
   Future<SearchResult> fetch(String keyword) async {
     // add calling API here, if needed
-    await Future<void>.delayed(const Duration(milliseconds: 300));
+    await Future<void>.delayed(const Duration(milliseconds: 1000));
+    final size = max(_random.nextInt(30) - 10, 0);
     return SearchResult(
       keyword: keyword,
-      list: List.generate(10, (index) => "'$keyword'の結果($index)"),
+      list: List.generate(size, (index) => "Result '$keyword'($index)"),
     );
   }
 }
