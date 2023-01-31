e23613663554e73cd400216dbfc03d340b81261a
k_senda <k_senda@yumemi.co.jp>
2023-01-31 14:40:27 +0900

    修正：ランダムにError表示

--- lib/data/repository/search_repository.dart
+++ lib/data/repository/search_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:async_test_sample/data/model/search_result.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final searchRepositoryProvider = Provider(
@@ -15,7 +16,12 @@ class SearchRepository {
   Future<SearchResult> fetch(String keyword) async {
     // add calling API here, if needed
     await Future<void>.delayed(const Duration(milliseconds: 1000));
-    final size = max(_random.nextInt(30) - 10, 0);
+    if (_random.nextDouble() < 0.2) {
+      debugPrint("throw exception");
+      throw Exception("test");
+    }
+    final size = max(_random.nextInt(20) - 5, 0);
+    debugPrint("fetch success size: $size");
     return SearchResult(
       keyword: keyword,
       list: List.generate(size, (index) => "Result '$keyword'($index)"),
